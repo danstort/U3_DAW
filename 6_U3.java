f489967986d2a64ec5a062d8971ebd0999a7640e
danstort <129515419+danstort@users.noreply.github.com>
2023-10-27 12:11:42 +0200

    Create 6_U3.java

---
+++ 6_U3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/**
+ *
+ * @author danielsotoortiz
+ */
+public class Ejercicio6 {
+
+    public static void main(String[] args) {
+        Scanner tec = new Scanner(System.in);
+        int a, b, n;
+        a=tec.nextInt();
+        b=tec.nextInt();
+        n=a+b;
+        System.out.print(n);
+        
+        }
+
+    }
