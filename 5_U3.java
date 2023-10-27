46f0e2de9767f7127279fd3a04446a7835849e8b
danstort <129515419+danstort@users.noreply.github.com>
2023-10-27 12:06:09 +0200

    Create 5_U3.java

---
+++ 5_U3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ *
+ * @author danielsotoortiz
+ */
+public class Ejercicio5 {
+
+    public static void main(String[] args) {
+        Scanner tec = new Scanner(System.in);
+        double a, b;
+        double n;
+        a=tec.nextInt();
+        b=tec.nextInt();
+        
+        n=a*(b/100);
+        
+        System.out.print(n);
+        
+        }
+
+    }
